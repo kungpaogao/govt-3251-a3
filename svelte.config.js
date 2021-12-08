b4604b8cf2b9ded10e8f7fdf12b473963ab5cf68
Andrew <kungpaogao@gmail.com>
2021-12-08 00:21:19 -0500

    package and config updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,25 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
+import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+    vite: {
+      plugins: [mdPlugin({ mode: [Mode.HTML, Mode.TOC] })]
+    }
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ]
 };
 
 export default config;
